ea803bdd149cd785fc742213714c9631648858c7
Frederik Ružový <frederikruzovy1@gmail.com>
2022-07-22 18:22:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
+import NavBar from "./components/layout/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar className=""></NavBar>
       </header>
     </div>
   );
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+export default class 
+ extends Component {
+  render() {
+    return (
+      <div>
+        <navbar className="navbar navbar-expand-md navbar-dark fixed-top">
+          <a className="navbar-brand" href="#">F1 ressults</a>
+        </navbar>
+      </div>
+    )
+  }
+}
