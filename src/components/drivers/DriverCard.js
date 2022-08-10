d057e1b1032141b21966a8ef10ee821ff42a31f1
Frederik Ružový <frederikruzovy1@gmail.com>
2022-08-10 16:30:37 +0200

    Add unique id to drivers card element for future use(Api doesnt have any imgs)

--- src/components/drivers/DriverCard.js
+++ src/components/drivers/DriverCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export default class DriverCard extends Component {
     state={
+        driver: '',
         familyName: '',
         givenName: '',
         position: '',
@@ -13,12 +14,14 @@ export default class DriverCard extends Component {
     render() {
         const familyName = this.props.familyName;
         const position = this.props.position;
-        const points = this.props.points
+        const points = this.props.points;
+        const code = this.props.code
         // const url = this.props.url;
+        // console.log(driver)
 
         return (
             <div className="col-md-3 col-sm-6 my-3">
-                <div className="card">
+                <div className="card" id={code}>
                     <div className="card-body">
                         <h5 className="card-title">{position}</h5>
                         <p className="card-text badge-danger">{points} points</p>
--- src/components/drivers/DriversList.js
+++ src/components/drivers/DriversList.js
@@ -13,8 +13,8 @@ export default class DriversList extends Component {
   async componentDidMount() {
     const res = await axios.get(this.state.url);
     let results = res.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
-    console.log(results);
-    console.log(results[0].Driver.familyName)
+    // console.log(results);
+    // console.log(results[0].Driver.familyName)
     this.setState({ drivers: results });
   }
 
@@ -26,7 +26,8 @@ export default class DriversList extends Component {
         <div className="row">
           {this.state.drivers.map(driver => (
             <DriverCard
-            key={driver.Driver.code}
+            key = {driver.Driver.code}
+            // driver = {driver}
             familyName = {driver.Driver.familyName}
             code = {driver.Driver.code}
             position = {driver.position}
