be1c75e295f4fe30f565ac24daf0f8fe98825fd4
Frederik Ružový <frederikruzovy1@gmail.com>
2022-08-22 15:06:17 +0200

    Rework header, Add logo

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
-export default class 
- extends Component {
+export default class
+  extends Component {
   render() {
     return (
       <div>
         <navbar className="navbar navbar-expand-md navbar-dark fixed-top">
-          <a className="navbar-brand col-sm-3 sol-md-2 mr-0 align-items-center ms-4" href="#">F1 results</a>
+          <div className="navbar-logo-skew"></div>
+          <div className="navbar-logo-area d-flex justify-content-start align-items-center">
+            <a className="navbar-brand col-sm-3 sol-md-2 mr-0 ms-3 align-self-end" href="#"></a>
+          </div>
         </navbar>
       </div>
     )
