6fdfb4103ac5a41e03bb7fe2331bef5d6d5b1eeb
Frederik Ružový <frederikruzovy1@gmail.com>
2022-10-13 20:59:12 +0200

    Fix overflow

--- src/components/layout/Dashboard.js
+++ src/components/layout/Dashboard.js
@@ -1,15 +1,12 @@
-import React, { Component } from 'react'
-import DriversList from '../drivers/DriversList'
+import React, { Component } from "react";
+import DriversList from "../drivers/DriversList";
 
-export default class
-    extends Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col">
-                    <DriversList />
-                </div>
-            </div>
-        )
-    }
+export default class extends Component {
+  render() {
+    return (
+      <div>
+        <DriversList />
+      </div>
+    );
+  }
 }
